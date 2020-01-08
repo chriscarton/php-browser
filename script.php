6e759806cfe7235af569c60ca36eab9c5fedfb3e
Chris <chriscarton@live.fr>
2020-01-08 20:18:59 +0100

    Ajout d'un système pour télécharger les fichiers selon les extensions présentes

--- script.php
+++ script.php
@@ -3,6 +3,33 @@
 if(!empty($_GET['f'])){
     $file = $_GET['f'];
 
+    $ext = pathinfo($file,PATHINFO_EXTENSION);
+
+    $file_without_ext = str_replace($ext,'',$file);
+    
+    $explode = explode('/',$file);
+    $filename = end($explode);
+    $folder = str_replace($filename,'',$file);
+
+    //print_r($_GET['f']);
+    
+    
+    $rii = new IteratorIterator(new DirectoryIterator($folder));
+
+    $other_files = [];
+    foreach($rii as $result){
+        if($result->isFile()){
+
+            $other_file = [];
+
+            $other_file['ext'] = $result->getExtension();
+            $other_file['filename'] = $result->getFilename();
+            $other_file['path'] = $result->getPath();
+            $other_file['fullpath'] = $result->getPath().'/'.$result->getFilename();
+
+            $other_files[] = $other_file;
+        }
+    }
     require_once('view.php');
 
 }else{
--- view.php
+++ view.php
@@ -1,7 +1,21 @@
-<?php
-//print_r($file);
-?>
-
 <div id="View">
+
+    <div class="files">
+        <?php 
+        
+        foreach($other_files as $other_file):
+            
+            if(file_exists($other_file['path'])):
+                
+                ?>
+                <a class="btn" download href="<?= $other_file['fullpath'] ?>">
+                    Télécharger le <?= $other_file['ext'] ?>
+                </a>
+                <?php
+            endif;
+            
+        endforeach;
+        ?>
+    </div>
     <img src="<?= $file ?>" alt="Image">
 </div>
